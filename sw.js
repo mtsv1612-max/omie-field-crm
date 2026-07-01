7f8f995d94b175bc3aa163150db91fdba6150504
mtsv1612-max <mtsv1612@gmail.com>
2026-07-01 09:21:55 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'omie-field-crm-public-v6';
+const CACHE_NAME = 'omie-field-crm-public-v7';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './sw.js', './icon.svg'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))); self.clients.claim(); });
